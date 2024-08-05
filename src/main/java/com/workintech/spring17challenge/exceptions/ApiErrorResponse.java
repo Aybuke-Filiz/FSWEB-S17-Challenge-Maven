08d23fc5bdfc3e2550e31a60bb02e4e1f6e09ff2
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-08-05 17:36:07 +0300

    görev tamamlandı.

---
+++ src/main/java/com/workintech/spring17challenge/exceptions/ApiErrorResponse.java
@@ -0,0 +1,16 @@
+package com.workintech.spring17challenge.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@Data
+public class ApiErrorResponse {
+  private Integer status;
+  private String message;
+  private Long timestamp;
+
+}
